93095992a6a51966d11a760f9d177673b906843b
Raymond Li <hi@raymond.li>
2020-09-01 17:51:37 -0400

    Main app done

--- app/src/main/java/li/raymond/goose/MainActivity.java
+++ app/src/main/java/li/raymond/goose/MainActivity.java
@@ -1,14 +1,37 @@
 package li.raymond.goose;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	long count = 10000;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	/**
+	 * Increment counter when Goose is clicked.
+	 */
+	public void incrementCounter(View view) {
+		count++;
+		TextView counter = (TextView)findViewById(R.id.counter);
+		counter.setText(Long.toString(count));
+	}
+
+	/**
+	 * Go to https://raymond.li when copyright is clicked
+	 */
+	public void visitSite(View view) {
+		Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://raymond.li"));
+		startActivity(browserIntent);
+	}
 }
