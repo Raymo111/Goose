aa7a4d1fbc4af9e200dae3eb94e0372deb9d9ad1
Raymond Li <hi@raymond.li>
2020-09-01 17:01:12 -0400

    Init app

---
+++ app/src/main/java/li/raymond/goose/MainActivity.java
@@ -0,0 +1,14 @@
+package li.raymond.goose;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
