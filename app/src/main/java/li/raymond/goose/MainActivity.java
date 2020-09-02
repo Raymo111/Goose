82e3ca929db3cbfd36c63b8e6bdfd6a8cd4f7a91
Raymond Li <hi@raymond.li>
2020-09-01 23:10:34 -0400

    Force portrait orientation and make counter persistent

--- app/src/main/java/li/raymond/goose/MainActivity.java
+++ app/src/main/java/li/raymond/goose/MainActivity.java
@@ -1,6 +1,8 @@
 package li.raymond.goose;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -10,12 +12,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-	long count = 10000;
+	SharedPreferences sharedPref;
+	long count;
+	SharedPreferences.Editor editor;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+		count = sharedPref.getLong(getString(R.string.counter), Long.parseLong(getString(R.string.default_counter)));
+		editor = sharedPref.edit();
 		setContentView(R.layout.activity_main);
+		TextView counter = (TextView) findViewById(R.id.counter);
+		counter.setText(Long.toString(count));
 	}
 
 	/**
@@ -23,15 +32,18 @@ public class MainActivity extends AppCompatActivity {
 	 */
 	public void incrementCounter(View view) {
 		count++;
-		TextView counter = (TextView)findViewById(R.id.counter);
+		TextView counter = (TextView) findViewById(R.id.counter);
 		counter.setText(Long.toString(count));
+		//if (count % 5 == 0) {
+		editor.putLong(getString(R.string.counter), count);
+		editor.apply();
+		//}
 	}
 
 	/**
 	 * Go to https://raymond.li when copyright is clicked
 	 */
 	public void visitSite(View view) {
-		Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://raymond.li"));
-		startActivity(browserIntent);
+		startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(getResources().getString(R.string.site))));
 	}
 }
